8a12e7cee88909dd1ce6062ea4da5cf0fddf1fb9
Pippoide <76634450+Pippoide@users.noreply.github.com>
2022-03-25 23:33:47 +0100

    API Google Book e sistema di ricerca
    finita l'aggiunta dell' API e del sistema di ricerca. Creata funzione di calcolo del numero di pagine disponibili

--- src/components/app.js
+++ src/components/app.js
@@ -1,38 +1,71 @@
 import { Component } from "react";
 import Card from "./card";
 import Axios from "axios";
-//https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key={process.env.API_KEY_BOOK}
-//https://www.youtube.com/watch?v=LGcgChoD_qY&t=570s
+
 class App extends Component {
     state = {
-        libreria: { nBook: 100, pages: 5 },
-        card: [
-            {id:1, titolo: "prova0", descrizione: "lorem-ipsum asdadads", img: "https://tse2.mm.bing.net/th?id=OIP.kUjnic5X18CUa1s6_BHTvgHaGL&w=690&c=7&pid=Api&p=0", link: "https://youtube.com" },
-            {id:2, titolo: "prova1", descrizione: "lorem-ipsum asdadads", img: "https://tse2.mm.bing.net/th?id=OIP.kUjnic5X18CUa1s6_BHTvgHaGL&w=690&c=7&pid=Api&p=0", link: "https://youtube.com" },
-            {id:3, titolo: "prova2", descrizione: "lorem-ipsum asdadads", img: "https://tse2.mm.bing.net/th?id=OIP.kUjnic5X18CUa1s6_BHTvgHaGL&w=690&c=7&pid=Api&p=0", link: "https://youtube.com" },
-            {id:4, titolo: "prova3", descrizione: "lorem-ipsum asdadads", img: "https://tse2.mm.bing.net/th?id=OIP.kUjnic5X18CUa1s6_BHTvgHaGL&w=690&c=7&pid=Api&p=0", link: "https://youtube.com" },
-            {id:5, titolo: "prova4", descrizione: "lorem-ipsum asdadads", img: "https://tse2.mm.bing.net/th?id=OIP.kUjnic5X18CUa1s6_BHTvgHaGL&w=690&c=7&pid=Api&p=0", link: "https://youtube.com" },
-        ]
+        viewMax:5,
+        ricerca:"",
+        card: [ ],
+        page: 8
     }
 
-    checkBook = () =>{
-
-        Axios.get("https://www.googleapis.com/books/v1/volumes?q=mela&key=apikey").then(data =>{
-            console.log(data.items);
-        })
+    checkBook = (ricerca) => {
+        Axios.get("https://www.googleapis.com/books/v1/volumes?q="+ricerca+"&key=" + process.env.REACT_APP_API_KEY_BOOK+"&maxResults=40").then(response => {
+        var cardNull = [];
+            this.setState({card:cardNull})
+            var desc;
+            console.log(response.data.items)
+            for (var i = 0; i < this.state.viewMax; i++) {
+                if (response.data.items[i].volumeInfo.description == undefined) {
+                    desc = "Descrizione non disponibile";
+                }
+                else{
+                    desc = response.data.items[i].volumeInfo.description
+                }
+                const book = {
+                    id: i,
+                    titolo: response.data.items[i].volumeInfo.title,
+                    descrizione: desc,
+                    img: response.data.items[i].volumeInfo.imageLinks.thumbnail,
+                    link: response.data.items[i].volumeInfo.infoLink
+                };
+                this.state.card.push(book);
+                this.setState({ricerca:ricerca})
+            }
+            console.log(this.state.card);
+        }
+        )
     }
-
+    giveInputValue = (event) => {
+       const ricerca = event.target.value;
+       this.checkBook(ricerca);
+    }
+    setViewmax = (view) =>{
+        this.setState({viewMax:view});
+        var x = 40/view;
+        //era brutto da vedere nella barra << |1|2|2| >>
+        //meglio avere << |1|2|max| >>
+        if(x==2)
+        {
+            x="max"
+        }
+        else{
+            x=x.toFixed()
+        }
+        this.setState({page:x});
+        this.checkBook(this.state.ricerca);
+    }
     render() {
         return (
             <div className='home d-flex flex-column p-0 m-0'>
                 <div className="home-nav d-flex flex-column justify-content-center align-items-center">
                     <nav className="navbar navbar-light p-0">
                         <div className=" container-fluid p-0 flex-column">
-                            <p className="h1 text-light fs-1 fw-bold mb-3 text-center">{process.env.REACT_APP_API_KEY_BOOK}Cerca un libro tramite l'API di Google Book</p>
-                            <form onChange={this.checkBook} className="d-flex flex-row align-items-center pt-3">
-                                <input className="home-nav-input d-flex text-center border-0 py-2 px-5 mx-2 fs-6 rounded-pill" type="search" placeholder="cerca il titolo del libro" aria-label="Search"></input>
-                                <button className="d-flex justify-content-center bi bi-search fw-bold fs-5 bg-light p-2 rounded-circle border-0" type="submit"></button>
-                            </form>
+                            <p className="h1 text-light fs-1 fw-bold mb-3 text-center">Cerca un libro tramite l'API di Google Book</p>
+                            <div className="d-flex flex-row align-items-center pt-3">
+                                <input onChange={(event)=>this.giveInputValue(event)} className="home-nav-input d-flex text-center border-0 py-2 px-5 mx-2 fs-6 rounded-pill" type="search" placeholder="cerca il titolo del libro" aria-label="Search"></input>
+                            </div>
                         </div>
                     </nav>
                 </div>
@@ -41,20 +74,20 @@ class App extends Component {
                         <div className='row row-cols-1 row-cols-md-3 m-0 px-5 pt-5 container-sm'>
                             {
                                 this.state.card.map((card) =>
-                                    <Card key={card.id} 
+                                    <Card key={card.id}
                                         titolo={card.titolo}
                                         descrizione={card.descrizione}
                                         img={card.img}
                                         link={card.link}
-                                        />
+                                    />
                                 )
                             }
                             <div className="col p-4">
                                 <div className="card flex-column d-flex justify-content-between aling-items-center h-100">
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit">lista da 5</button>
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit">lista da 10</button>
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit">lista da 15</button>
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit">lista da 20</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(5)}>lista da 5</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(10)}>lista da 10</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(15)}>lista da 15</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(20)}>lista da 20</button>
                                 </div>
                             </div>
                         </div>
@@ -66,9 +99,9 @@ class App extends Component {
                                             <span aria-hidden="true" className='text-black'>&laquo;</span>
                                         </a>
                                     </li>
-                                    <li className="page-item"><a className="page-link text-black" href="#">1</a></li>
-                                    <li className="page-item"><a className="page-link text-black" href="#">2</a></li>
-                                    <li className="page-item"><a className="page-link text-black" href="#">3</a></li>
+                                    <li className="page-item"><a className="page-link text-black" >1</a></li>
+                                    <li className="page-item"><a className="page-link text-black" >2</a></li>
+                                    <li className="page-item"><a className="page-link text-black" >{this.state.page}</a></li>
                                     <li className="page-item">
                                         <a className="page-link" href="#" aria-label="Next">
                                             <span aria-hidden="true" className='text-black'>&raquo;</span>
--- src/components/card.js
+++ src/components/card.js
@@ -5,12 +5,12 @@ class Card extends Component {
 
     render() {
         return (
-            <div className="col p-4">
-            <a className="card text-black text-decoration-none" target="_blank" href={this.props.link}>
+            <div className="col p-4" style={{minHeight:"300px"}}>
+            <a className="card text-black text-decoration-none h-100" target="_blank" href={this.props.link}>
                 <img src={this.props.img} className="card-img-top" alt="..."></img>
                 <div className="card-body rounded-pill">
                     <h5 className="card-title text-capitalize">{this.props.titolo}</h5>
-                    <p className="word-break card-text">
+                    <p className="card-text " style={{textOverflow:"ellipsis",maxHeight:"100px",overflow:"hidden",whiteSpace:"nowrap"}}>
                         {this.props.descrizione}
                     </p>
                 </div>
