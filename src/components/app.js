ad3e30349d4dd53ea89d78541b5770c2e56414e7
Pippoide <76634450+Pippoide@users.noreply.github.com>
2022-03-26 19:00:34 +0100

    Sistema di ricerca e paginazione
    progetto terminato, paginazione finita, visualizzazione a blocchi finita

--- src/components/app.js
+++ src/components/app.js
@@ -4,23 +4,26 @@ import Axios from "axios";
 
 class App extends Component {
     state = {
-        viewMax:5,
-        ricerca:"",
-        card: [ ],
-        page: 8
+        viewMaxBook: 5,
+        ricerca: "",
+        card: [],
+        pageMax: 8,
+        indexPage: 0
     }
 
     checkBook = (ricerca) => {
-        Axios.get("https://www.googleapis.com/books/v1/volumes?q="+ricerca+"&key=" + process.env.REACT_APP_API_KEY_BOOK+"&maxResults=40").then(response => {
-        var cardNull = [];
-            this.setState({card:cardNull})
+        Axios.get("https://www.googleapis.com/books/v1/volumes?q=" + ricerca + "&key=" + process.env.REACT_APP_API_KEY_BOOK + "&maxResults=40").then(response => {
+            var cardNull = [];
+            this.setState({ card: cardNull })
             var desc;
             console.log(response.data.items)
-            for (var i = 0; i < this.state.viewMax; i++) {
+            var indexMax = this.state.indexPage + this.state.viewMaxBook
+            console.log(indexMax)
+            for (var i = this.state.indexPage; i < indexMax; i++) {
                 if (response.data.items[i].volumeInfo.description == undefined) {
                     desc = "Descrizione non disponibile";
                 }
-                else{
+                else {
                     desc = response.data.items[i].volumeInfo.description
                 }
                 const book = {
@@ -31,30 +34,74 @@ class App extends Component {
                     link: response.data.items[i].volumeInfo.infoLink
                 };
                 this.state.card.push(book);
-                this.setState({ricerca:ricerca})
+                this.setState({ ricerca: ricerca })
             }
             console.log(this.state.card);
         }
         )
     }
     giveInputValue = (event) => {
-       const ricerca = event.target.value;
-       this.checkBook(ricerca);
+        const ricerca = event.target.value;
+        this.checkBook(ricerca);
     }
-    setViewmax = (view) =>{
-        this.setState({viewMax:view});
-        var x = 40/view;
-        //era brutto da vedere nella barra << |1|2|2| >>
-        //meglio avere << |1|2|max| >>
-        if(x==2)
-        {
-            x="max"
-        }
-        else{
-            x=x.toFixed()
-        }
-        this.setState({page:x});
+    setViewmax = (view) => {
+        this.setState({ viewMaxBook: view });
+        this.setState({ indexPage: 0 })
+        var x = 40 / view;
+        x = x.toFixed()
+        this.setState({ pageMax: x });
         this.checkBook(this.state.ricerca);
+    }
+    setPageIndex = (index) => {
+        //-1 = commando di pagina indientro
+        //-2 = commando di pagina avanti
+        if (this.state.viewMaxBook = 15) {
+            if(index==2){
+                this.setState({indexPage:15})
+            }
+            else{
+                if(index==3){
+                    this.setState({indexPage:30})
+                }
+                else{
+                    var startIndexView = (index * this.state.viewMaxBook)
+                    if (startIndexView >= 39) {
+                        startIndexView -= this.state.viewMaxBook
+                    }
+                    this.setState({ indexPage: startIndexView })
+                }
+        }
+        }
+        else {
+            switch (index) {
+                case -1:
+                    var x = this.state.indexPage - this.state.viewMaxBook;
+                    if (x >= 0) {
+                        this.setState({ indexPage: x })
+                    }
+                    else {
+                        global.alert("sono questi i primi risultati")
+                    }
+                    break;
+                case -2:
+                    var x = this.state.indexPage + this.state.viewMaxBook;
+                    if (x >= 39) {
+                        global.alert("non ci sono altri risultati diponibili")
+                    }
+                    else {
+                        this.setState({ indexPage: x })
+                    }
+                    break;
+                default:
+                    var startIndexView = (index * this.state.viewMaxBook)
+                    if (startIndexView >= 39) {
+                        startIndexView -= this.state.viewMaxBook
+                    }
+                    this.setState({ indexPage: startIndexView })
+                    break;
+            }
+        }
+        this.checkBook(this.state.ricerca)
     }
     render() {
         return (
@@ -64,7 +111,7 @@ class App extends Component {
                         <div className=" container-fluid p-0 flex-column">
                             <p className="h1 text-light fs-1 fw-bold mb-3 text-center">Cerca un libro tramite l'API di Google Book</p>
                             <div className="d-flex flex-row align-items-center pt-3">
-                                <input onChange={(event)=>this.giveInputValue(event)} className="home-nav-input d-flex text-center border-0 py-2 px-5 mx-2 fs-6 rounded-pill" type="search" placeholder="cerca il titolo del libro" aria-label="Search"></input>
+                                <input onChange={(event) => this.giveInputValue(event)} className="home-nav-input d-flex text-center border-0 py-2 px-5 mx-2 fs-6 rounded-pill" type="search" placeholder="cerca il titolo del libro" aria-label="Search"></input>
                             </div>
                         </div>
                     </nav>
@@ -84,10 +131,10 @@ class App extends Component {
                             }
                             <div className="col p-4">
                                 <div className="card flex-column d-flex justify-content-between aling-items-center h-100">
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(5)}>lista da 5</button>
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(10)}>lista da 10</button>
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(15)}>lista da 15</button>
-                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={()=>this.setViewmax(20)}>lista da 20</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={() => this.setViewmax(5)}>lista da 5</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={() => this.setViewmax(10)}>lista da 10</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={() => this.setViewmax(15)}>lista da 15</button>
+                                    <button className="h-25 w-auto rounded m-2 p-0 d-flex justify-content-center align-items-center fs-5 bg-light p-2 border-0 w-100" type="submit" onClick={() => this.setViewmax(20)}>lista da 20</button>
                                 </div>
                             </div>
                         </div>
@@ -95,15 +142,15 @@ class App extends Component {
                             <nav aria-label="Page navigation example" className='d-flex justify-content-center align-items-center'>
                                 <ul className="pagination">
                                     <li className="page-item">
-                                        <a className="page-link" href="#" aria-label="Previous">
+                                        <a className="page-link" onClick={(index) => this.setPageIndex(-1)} aria-label="Previous">
                                             <span aria-hidden="true" className='text-black'>&laquo;</span>
                                         </a>
                                     </li>
-                                    <li className="page-item"><a className="page-link text-black" >1</a></li>
-                                    <li className="page-item"><a className="page-link text-black" >2</a></li>
-                                    <li className="page-item"><a className="page-link text-black" >{this.state.page}</a></li>
+                                    <li className="page-item"><a className="page-link text-black" onClick={(index) => this.setPageIndex(0)}>1</a></li>
+                                    <li className="page-item"><a className="page-link text-black" onClick={(index) => this.setPageIndex(2)}>2</a></li>
+                                    <li className="page-item"><a className="page-link text-black" onClick={(index) => this.setPageIndex((this.state.pageMax))}>{(this.state.pageMax)}</a></li>
                                     <li className="page-item">
-                                        <a className="page-link" href="#" aria-label="Next">
+                                        <a className="page-link" onClick={(index) => this.setPageIndex(-2)} aria-label="Next">
                                             <span aria-hidden="true" className='text-black'>&raquo;</span>
                                         </a>
                                     </li>
